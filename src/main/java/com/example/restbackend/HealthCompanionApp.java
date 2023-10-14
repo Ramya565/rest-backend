361d7cf2d47adb3303dc782162607ddfbbd9f052
user <example@gmail.com>
2023-10-14 18:15:50 +0000

    HealthCompanionApp

--- src/main/java/com/example/restbackend/HealthCompanionApp.java
+++ src/main/java/com/example/restbackend/HealthCompanionApp.java
@@ -1,4 +1,116 @@
 package com.example.restbackend;
 
 public class HealthCompanionApp {
+    String EarlySymptomDetection;
+    String PreventionOverIntervention;
+    String AyurvedicPractices;
+    String PersonalizedGuidance;
+    String SelfAdvocacy;
+    String DietAndExercisePlans;
+    String MotivationalContent;
+    String Education;
+    String ComprehensiveKnowledge;
+    String HolisticApproach;
+
+    public HealthCompanionApp(String earlySymptomDetection,
+                              String preventionOverIntervention,
+                              String ayurvedicPractices,
+                              String personalizedGuidance,
+                              String selfAdvocacy,
+                              String dietAndExercisePlans,
+                              String motivationalContent,
+                              String education,
+                              String comprehensiveKnowledge,
+                              String holisticApproach) {
+        EarlySymptomDetection = earlySymptomDetection;
+        PreventionOverIntervention = preventionOverIntervention;
+        AyurvedicPractices = ayurvedicPractices;
+        PersonalizedGuidance = personalizedGuidance;
+        SelfAdvocacy = selfAdvocacy;
+        DietAndExercisePlans = dietAndExercisePlans;
+        MotivationalContent = motivationalContent;
+        Education = education;
+        ComprehensiveKnowledge = comprehensiveKnowledge;
+        HolisticApproach = holisticApproach;
+    }
+
+    public String getEarlySymptomDetection() {
+        return EarlySymptomDetection;
+    }
+
+    public void setEarlySymptomDetection(String earlySymptomDetection) {
+        EarlySymptomDetection = earlySymptomDetection;
+    }
+
+    public String getPreventionOverIntervention() {
+        return PreventionOverIntervention;
+    }
+
+    public void setPreventionOverIntervention(String preventionOverIntervention) {
+        PreventionOverIntervention = preventionOverIntervention;
+    }
+
+    public String getAyurvedicPractices() {
+        return AyurvedicPractices;
+    }
+
+    public void setAyurvedicPractices(String ayurvedicPractices) {
+        AyurvedicPractices = ayurvedicPractices;
+    }
+
+    public String getPersonalizedGuidance() {
+        return PersonalizedGuidance;
+    }
+
+    public void setPersonalizedGuidance(String personalizedGuidance) {
+        PersonalizedGuidance = personalizedGuidance;
+    }
+
+    public String getSelfAdvocacy() {
+        return SelfAdvocacy;
+    }
+
+    public void setSelfAdvocacy(String selfAdvocacy) {
+        SelfAdvocacy = selfAdvocacy;
+    }
+
+    public String getDietAndExercisePlans() {
+        return DietAndExercisePlans;
+    }
+
+    public void setDietAndExercisePlans(String dietAndExercisePlans) {
+        DietAndExercisePlans = dietAndExercisePlans;
+    }
+
+    public String getMotivationalContent() {
+        return MotivationalContent;
+    }
+
+    public void setMotivationalContent(String motivationalContent) {
+        MotivationalContent = motivationalContent;
+    }
+
+    public String getEducation() {
+        return Education;
+    }
+
+    public void setEducation(String education) {
+        Education = education;
+    }
+
+    public String getComprehensiveKnowledge() {
+        return ComprehensiveKnowledge;
+    }
+
+    public void setComprehensiveKnowledge(String comprehensiveKnowledge) {
+        ComprehensiveKnowledge = comprehensiveKnowledge;
+    }
+
+    public String getHolisticApproach() {
+        return HolisticApproach;
+    }
+
+    public void setHolisticApproach(String holisticApproach) {
+        HolisticApproach = holisticApproach;
+    }
 }
